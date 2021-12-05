3235fdfe4ca93c3dcddac04c5095f45ac2a391dc
Mats Krüger Svensson <mats@matsemann.com>
2021-12-05 13:13:34 +0100

    aoc2021 day 5

--- adventofcode2021/src/main/kotlin/com/matsemann/adventofcode2021/util.kt
+++ adventofcode2021/src/main/kotlin/com/matsemann/adventofcode2021/util.kt
@@ -1,7 +1,13 @@
 package com.matsemann.adventofcode2021
 
+import java.awt.Toolkit
+import java.awt.datatransfer.StringSelection
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.sign
+import kotlin.math.sqrt
 
 
 fun getFileLines(fileName: String) =
@@ -11,14 +17,149 @@ fun run(runName: String? = null, fileName: String, func: (List<String>) -> Any) 
     val lines = getFileLines(fileName)
     val result = measure { func(lines) }
 
-    println("Result for ${runName ?: fileName}:\t${result}\n")
+    println("Result for ${runName ?: fileName}:\t${result}")
+    try {
+        Toolkit.getDefaultToolkit().systemClipboard.setContents(StringSelection(result.toString()), null)
+        println("Copied to clipboard!\n")
+    } catch (_: Exception) {
+
+    }
 }
 
-fun <T> measure(func: () -> T) : T {
+fun <T> measure(func: () -> T): T {
     val start = System.currentTimeMillis()
 
     return func().also {
         val time = System.currentTimeMillis() - start
         println("Done. Took ${time}ms to run")
+    }
+}
+
+
+enum class Direction(var x: Int, var y: Int) {
+    RIGHT(1, 0),
+    DOWN(0, -1),
+    LEFT(-1, 0),
+    UP(0, 1);
+
+    fun turnCw() =
+        when (this) {
+            RIGHT -> DOWN
+            DOWN -> LEFT
+            LEFT -> UP
+            UP -> RIGHT
+        }
+
+    fun turnCcw() =
+        when (this) {
+            RIGHT -> UP
+            DOWN -> RIGHT
+            LEFT -> DOWN
+            UP -> LEFT
+        }
+
+    fun flip() =
+        when(this) {
+            RIGHT -> LEFT
+            LEFT -> RIGHT
+            UP -> DOWN
+            DOWN -> UP
+        }
+
+    companion object {
+        fun fromNSEW(ch: Char) {
+            when (ch) {
+                'N' -> UP
+                'S' -> DOWN
+                'E' -> RIGHT
+                'W' -> LEFT
+            }
+        }
+
+        fun fromUDLR(ch: Char) {
+            when (ch) {
+                'U' -> UP
+                'D' -> DOWN
+                'R' -> RIGHT
+                'L' -> LEFT
+            }
+        }
+
+        fun setYDown() {
+            DOWN.y = 1
+            UP.y = -1
+        }
+    }
+}
+
+data class Vec2d(val x: Double, val y: Double) {
+    operator fun plus(other: Vec2d) = Vec2d(x + other.x, y + other.y)
+    operator fun plus(other: IntVec) = Vec2d(x + other.x, y + other.y)
+    operator fun plus(dir: Direction) = Vec2d(x + dir.x, y + dir.y)
+
+    operator fun minus(other: Vec2d) = Vec2d(x - other.x, y - other.y)
+    operator fun unaryMinus() = Vec2d(-x, -y)
+
+    operator fun times(factor: Double) = Vec2d(x * factor, y * factor)
+
+    fun manhattan(other: Vec2d) = abs(x - other.x) + abs(y - other.y)
+    fun dst(other: Vec2d) = sqrt( (x - other.x) * (x - other.x) + (y - other.y) * (y - other.y))
+    fun len() = dst(zero)
+    fun norm() = times(1 / len())
+    fun dot(other: Vec2d) = x * other.x + y * other.y
+    fun cross(other: Vec2d) = x * y - y * x
+
+    fun isPerpendicular(other: Vec2d)  = abs(dot(other)) < 0.000001
+    fun hasSameDirection(other: Vec2d)  = dot(other) > 0.0
+    fun hasOppositeDirection(other: Vec2d)  = dot(other) < 0.0
+
+    companion object {
+        val zero = Vec2d(0.0, 0.0)
+    }
+}
+
+
+data class IntVec(val x: Int, val y: Int) {
+    operator fun plus(other: IntVec) = IntVec(x + other.x, y + other.y)
+    operator fun plus(dir: Direction) = IntVec(x + dir.x, y + dir.y)
+
+    operator fun minus(other: IntVec) = IntVec(x - other.x, y - other.y)
+    operator fun unaryMinus() = IntVec(-x, -y)
+
+    operator fun times(factor: Int) = IntVec(x * factor, y * factor)
+
+    fun manhattan(other: IntVec) = abs(x - other.x) + abs(y - other.y)
+    fun manhattan() = manhattan(zero)
+    fun chebyshev(other: IntVec) = max(abs(x - other.x), abs(y - other.y))
+    fun chebyshev() = chebyshev(zero)
+
+    fun asDir() = IntVec(x.sign, y.sign)
+
+    // Note, it is inclusive, so for indexing reduce with 1
+    fun withinBounds(bounds: IntVec) = withinBounds(0, bounds.x, 0, bounds.y)
+    fun withinBounds(minX: Int, maxX: Int, minY: Int, maxY: Int) =
+        x in minX..maxX && y in minY..maxY
+
+    fun neighbors() = Direction.values().map { this + it }
+    fun neighbors(bounds: IntVec) = Direction.values().map { this + it }
+        .filter { it.withinBounds(bounds) }
+
+    fun neighbors9() = (-1..1)
+        .flatMap { x ->
+            (-1..1)
+                .map { y -> IntVec(x, y) }
+        }
+        .filter { it.x != 0 || it.y != 0 }
+        .map { this + it }
+
+    companion object {
+        val zero = IntVec(0, 0)
+
+        fun fromStr(str: String, delim: String = ","): IntVec {
+            val split = str.split(delim)
+            return IntVec(split[0].toInt(), split[1].toInt())
+        }
+
+        fun String.toIntVec(delim: String = ",")  = fromStr(this, delim)
     }
 }
