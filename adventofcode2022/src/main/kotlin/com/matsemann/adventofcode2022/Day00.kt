2a2d68f44911d956cc11bccb0818ee1d795f07b5
Mats Krüger Svensson <mats@matsemann.com>
2022-12-01 08:38:24 +0100

    aoc day1 2022

---
+++ adventofcode2022/src/main/kotlin/com/matsemann/adventofcode2022/Day00.kt
@@ -0,0 +1,30 @@
+package com.matsemann.adventofcode2022
+
+
+fun day00_1(lines: List<String>): Any {
+    lines.forEach { println(it)}
+    return 12223345
+}
+
+// curl https://adventofcode.com/2022/day/0/input --cookie "session=53616c7465645f5f2630ea26d062e2de9f48908da3c3bbaaf15c6db74787d57c5ad60d335d8edfb91c1ab38b67a8376cae6e511e09ee56fc16421161991e86ea" > inputs/day00.txt
+
+
+fun day00_2(lines: List<String>): Any {
+    return 2
+}
+
+fun main() {
+
+//    run("1", fileName = "day00_ex.txt", func = ::day00_1)
+//    run("2", fileName = "day00_ex.txt", func = ::day00_2)
+
+    run("1", fileName = "day00.txt", func = ::day00_1)
+//    run("2", fileName = "day00.txt", func = ::day00_2)
+}
+
+/*
+OUTPUT
+======
+
+
+ */
---
+++ adventofcode2022/src/main/kotlin/com/matsemann/adventofcode2022/Day01.kt
@@ -0,0 +1,28 @@
+package com.matsemann.adventofcode2022
+
+fun day01_1(lines: List<String>): Any {
+    return lines
+        .splitBy { it == "" }
+        .map { it.ints().sum() }
+        .max()
+}
+
+
+fun day01_2(lines: List<String>): Any {
+    return lines
+        .splitBy { it == "" }
+        .map { it.ints().sum() }
+        .sortedDescending()
+        .take(3)
+        .sum()
+}
+
+fun main() {
+
+//    run("1", fileName = "day01_ex.txt", func = ::day01_1)
+//    run("2", fileName = "day01_ex.txt", func = ::day01_2)
+
+
+    run("1", fileName = "day01.txt", func = ::day01_1)
+    run("2", fileName = "day01.txt", func = ::day01_2)
+}
